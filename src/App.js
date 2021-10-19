c204c9f15446f1b565e77e5c73dde36705a43ec3
Wookiee17 <90505087+Wookiee17@users.noreply.github.com>
2021-10-19 23:39:33 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FileUploader from 'devextreme-react/file-uploader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { chunks: [] };
+    this.onUploadProgress = this.onUploadProgress.bind(this);
+    this.onUploadStarted = this.onUploadStarted.bind(this);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <FileUploader name="file" accept="image/*" uploadUrl="https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/ChunkUpload"
+          chunkSize={4194304} onUploadStarted={this.onUploadStarted} onProgress={this.onUploadProgress} />
+        <span className="note">Allowed file extensions: <span>.jpg, .jpeg, .gif, .png</span>.</span>
+        <span className="note">Maximum file size: <span>4 MB.</span></span>
+        <div className="chunk-panel">
+          {
+            this.state.chunks.map((c, i) => {
+              return <div key={i}>
+                <span>Chunk size:</span>
+                <span className="segment-size dx-theme-accent-as-text-color">{this.getValueInKb(c.segmentSize)}</span>
+                <span>, Uploaded:</span>
+                <span className="loaded-size dx-theme-accent-as-text-color">{this.getValueInKb(c.bytesLoaded)}</span>
+                <span>/</span>
+                <span className="total-size dx-theme-accent-as-text-color">{this.getValueInKb(c.bytesTotal)}</span>
+              </div>;
+            })
+          }
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  onUploadProgress(e) {
+    const chunk = {
+      segmentSize: e.segmentSize,
+      bytesLoaded: e.bytesLoaded,
+      bytesTotal: e.bytesTotal
+    };
+    this.setState({ chunks: [...this.state.chunks, chunk] });
+  }
+
+  onUploadStarted() {
+    this.setState({ chunks: [] });
+  }
+
+  getValueInKb(value) {
+    return `${(value / 1024).toFixed(0)}kb`;
+  }
 }
 
 export default App;
